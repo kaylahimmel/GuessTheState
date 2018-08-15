0ea433f260cfbac79cab61257b1984cd6c36bf52
Kayla Himmelberger <kaylahimmelberger@Edwards-MacBook-Pro.local>
2018-08-15 11:14:51 -0400

    psuedo-coded out html, css, and js sheets with my plan of attack.

---
+++ Assets/javascript/game.js
@@ -0,0 +1,9 @@
+// need to add prompts that say whether the user won or lost that round
+
+// need the next round to start as soon as the user closes the propmt
+
+// need to use counter that keep track of wins and losses on the screen
+
+// need to use another counter than counts down the number of wrong guesses (from 10 or 12 maybe?)
+
+// if using music, need to figure out how to assign it (and where it goes)
