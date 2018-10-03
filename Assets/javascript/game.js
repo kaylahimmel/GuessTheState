d547c10f960d3955f071119b3ac124d9e09c120c
Kayla Himmelberger <kaylahimmelberger@gmail.com>
2018-10-02 23:00:21 -0400

    may have updated JS or CSS but can't remember for sure so am pushing just in case.

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,67 +2,40 @@
 // array of states (aka my puzzle options)
 var states = ["florida", "montana", "hawaii", "alaska", "texas", "wisconsin", "georgia", "alabama", 
                 "oregon", "idaho", "oklahoma", "ohio", "minnesota", "maine", "vermont", "arizona", 
-                "louisiana", "tennessee", "massachusetts", "delaware", "nebraska", "utah", "iowa"];
+                "louisiana", "tennessee", "arkansas", "delaware", "nebraska", "utah", "iowa"];
 
 // each round, set:
-var wins = 0
-var losses = 0
-var remainingGuesses = 10
-var puzzle = []
-var ranState;
-var guessedLetters = []
+var wins = 0;
+var losses = 0;
+var remainingGuesses = 10;
+var puzzle = [];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var guessedLetters = [];
+var wrongGuesses = [];
 var ranState = Math.floor(Math.random() * states.length);
 var chosenState = states[ranState];
 console.log(chosenState);
-
-
-// START THE GAME (AND RESTART ROUNDS)----------------------------------------------------------------
+    
+// Create an array of underscores
 var startGame = function() {
-    // Create our array of underscores
     for (var i = 0; i < chosenState.length; i++) {
         puzzle.push("_")
     }
-
-    console.log(chosenState.length)
-    console.log(puzzle.length)
-
-    document.getElementById("wins").textContent = wins;
-    document.getElementById("losses").textContent = losses;
-    document.getElementById("underscore").textContent = puzzle.join(" ");
-    console.log("Game is started")
-}
-
-
-// RESET THE GAME-------------------------------------------------------------------------------------
-var resetGame = function(remainingGuesses = maxGuesses) {
-    gameStarted = false;
-}
-
+    updateDisplay();
 
 //  Update the display on the HTML Page
 function updateDisplay() {
     document.getElementById("wins").textContent = wins;
     document.getElementById("losses").textContent = losses;
     document.getElementById("underscore").textContent = puzzle.join(" ");
+    document.getElementById("guesses-left").textContent = remainingGuesses;
+    document.getElementById("guesses").textContent = guessedLetters.join(", ")
 };
 
-// THE ACTUAL GAME
-// Capture user's key presses
-document.onkeyup = function(event) {
-    var letter = event.key
-    // Checking to see if the user has already guessed a certain letter
-    if (guessedLetters.indexOf(letter) === -1) {
-        guessedLetters.push(letter);
-        letterInstances(letter);
-    }
-    checkWin();
-}
-
-// The scoreboard
+// Add wins/losses to the scoreboard area
 function checkWin() {
-    console.log(puzzle)
     if(puzzle.indexOf("_") === -1) {
-        alert("You win!")
+        alert("You win!");
         wins++;   
         resetGame();
     } else if (remainingGuesses <= 0) {
@@ -82,5 +55,30 @@ function letterInstances(letter) {
     }
 };
 
-// Start the game
-startGame();
+// Capture user's key presses
+document.onkeyup = function(event) {
+    var letter = event.key
+    // Checking to see if the user has already guessed a certain letter
+    if (alphabet.indexOf(letter) >= 0) {
+        if (guessedLetters.indexOf(letter) === -1) {
+        guessedLetters.push(letter);
+        letterInstances(letter);
+        remainingGuesses--;
+        document.getElementById("prompt").textContent = "";
+        } else {
+        document.getElementById("prompt").textContent = "You've already guessed that letter";
+        }
+    } else {
+    document.getElementById("prompt").textContent = "You've must guess a letter";
+    }
+    checkWin();
+};
+
+// RESET THE GAME
+var resetGame = function() {
+    remainingGuesses = 10
+    puzzle = []
+    guessedLetters = []
+    ranState();
+    startGame();
+};
